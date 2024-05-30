a9cdb11e3dd956911139f6ee761a237d877db44c
Wanderson Rodrigues <147121602+wandersonrodriguespro@users.noreply.github.com>
2024-05-30 17:05:36 -0300

    create repository

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.export = {
+  dialect: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'devburguer',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
