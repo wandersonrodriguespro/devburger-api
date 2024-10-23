57554160e187a6f9e00d6616187bd685c65592f2
Wanderson Rodrigues <147121602+wandersonrodriguespro@users.noreply.github.com>
2024-10-23 18:27:14 -0300

    update Category Controller

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -21,6 +21,8 @@ class CategoryController {
       return response.status(401).json();
     }
 
+    const { filename: path } = request.file;
+
     const { name } = request.body;
 
     const categoryExists = await Category.findOne({ where: { name } });
@@ -29,9 +31,61 @@ class CategoryController {
       return response.status(400).json({ error: 'Category already exists' });
     }
 
-    const { id } = await Category.create({ name });
+    const { id } = await Category.create({ name, path });
 
     return response.status(201).json({ id, name });
+  }
+
+  async update(request, response) {
+    const schema = Yup.object({
+      name: Yup.string(),
+    });
+
+    try {
+      schema.validateSync(request.body, { abortEarly: false });
+    } catch (err) {
+      return response.status(400).json({ error: err.errors });
+    }
+
+    const { admin: isAdmin } = await User.findByPk(request.userId);
+    if (!isAdmin) {
+      return response.status(401).json();
+    }
+
+    const { id } = request.params;
+    const categoryExists = await Category.findByPk(id);
+    if (!categoryExists) {
+      return response.status(400).json({ message: 'Make sure your category ID is correct' });
+    }
+
+    let path;
+    if (request.file) {
+      path = request.file.filename;
+    }
+
+    const { name } = request.body;
+
+    if (name) {
+      const categoryNameExists = await Category.findOne({ where: { name } });
+
+      if (categoryNameExists && categoryNameExists.id !== +id) {
+        return response.status(400).json({ error: 'Category already exists' });
+      }
+    }
+
+    await Category.update(
+      {
+        name,
+        path,
+      },
+      {
+        where: {
+          id,
+        },
+      },
+    );
+
+    return response.status(200).json();
   }
 
   async index(request, response) {
--- src/routes.js
+++ src/routes.js
@@ -21,8 +21,9 @@ routes.post('/products', upload.single('file'), ProductController.store);
 routes.get('/products', authMiddleware, ProductController.index);
 routes.put('/products/:id', upload.single('file'), ProductController.update);
 
-routes.post('/categories', CategoryController.store);
+routes.post('/categories', upload.single('file'), CategoryController.store);
 routes.get('/categories', CategoryController.index);
+routes.put('/categories/:id', upload.single('file'), CategoryController.update);
 
 routes.post('/orders', OrderControler.store);
 routes.get('/orders', OrderControler.index);
